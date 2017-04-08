17d70bf60da6a84c2ba9e762c546541987590309
Griffin Nozell <griffin.nozell@plexxi.com>
2017-04-07 21:56:25 -0400

    Spit is now falling

---
+++ Assets/spit_controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spit_controller : MonoBehaviour {
+	float minimum;
+	float maximum;
+
+	public GameObject spittip;
+	LineRenderer lRender;
+
+	// Use this for initialization
+	void Start () {
+		lRender = GetComponent<LineRenderer> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 oldPosition = lRender.GetPosition (lRender.numPositions-1);
+		lRender.SetPosition (lRender.numPositions-1, oldPosition + new Vector3(0,-1*Time.deltaTime,0));
+
+
+		spittip.transform.position = lRender.GetPosition (lRender.numPositions -1);
+	}
+}
---
+++ Assets/z_object.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class z_object : MonoBehaviour {
+
+	Vector3 _startPosition;
+	public Sprite z1;
+	public Sprite z2;
+	public Sprite z3;
+	public GameObject myself;
+
+	SpriteRenderer spRender;
+
+	float timer = 0.0f;
+	float minimum;
+	float maximum;
+	// Use this for initialization
+	void Start () {
+		_startPosition = transform.position;
+		minimum = transform.position.y;
+		maximum = transform.position.y + 3f;
+		spRender = GetComponent<SpriteRenderer> ();
+		spRender.sprite = z1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(transform.position);
+		Debug.Log ("max:" + maximum.ToString() + " min:" + minimum.ToString());
+
+		//update sprite if at (1/3) hight and if at (2/3) height
+		if (transform.position.y > minimum + ((maximum - minimum)/3)*2) {
+			spRender.sprite = z3;
+		} else if (transform.position.y > minimum + ((maximum - minimum)/3) ) {
+			spRender.sprite = z2;
+		}
+
+		// kill myself if i ever reach the maximum
+		if (transform.position.y >= maximum){
+			
+			Instantiate (myself, new Vector3(0,0,0), Quaternion.identity);
+			Destroy (gameObject);
+		}
+
+		// update the position of the Z
+		transform.position = _startPosition + new Vector3(Mathf.Sin(Time.time), Mathf.Lerp(minimum, maximum, timer), 0.0f);
+		timer += 0.5f * Time.deltaTime;
+	}
+}
